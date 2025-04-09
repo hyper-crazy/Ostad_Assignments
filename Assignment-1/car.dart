045f37dc54bd2f64d11cff8e07446446c0946866
hyper-crazy <morshedulislammaurf00@gmail.com>
2025-04-10 00:54:52 +0600

    why used  insted of  reason added

--- Assignment-1/car.dart
+++ Assignment-1/car.dart
@@ -5,6 +5,6 @@ class Car extends Vehicle {
   @override
   void move() {
     // implementing move method
-    print('The car is moving at $speed km/hrs');
+    print('The car is moving at $speed km/hrs'); //calling the getter $speed insted using $_speed.
   }
 }
